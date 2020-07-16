146921284cbff7f668008cba336e3ccc783b3a11
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-17 01:11:56 +0530

    add TransactionList comp to app comp

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Components/Header';
 import Balance from './Components/Balance';
 import IncomeExpenses from './Components/IncomeExpenses';
+import TransactionList from './Components/TransactionList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Balance />
       <IncomeExpenses />
+      <TransactionList />
     </div>
   );
 }
