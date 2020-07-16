957fdc27103cfcf5fb66c3d3179e11cc56d5d7fe
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-17 01:17:10 +0530

    created AddTransaction comp

---
+++ src/Components/AddTransaction.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function AddTransaction() {
+    return (
+        <>
+            <h3>Add new transaction</h3>
+            <form>
+                <div className="form-control">
+                    <label htmlFor='text'>Text</label>
+                    <input type="text" placeholder="Enter text..." />
+                </div>
+                <div className="form-control">
+                    <label htmlFor='amount'>
+                        Amount <br />
+                        (negative - expense, positive - income)
+                    </label>
+                    <input type="number" placeholder="Enter amount..." />
+                </div>
+            </form>  
+        </>
+    )
+}
+
+export default AddTransaction
