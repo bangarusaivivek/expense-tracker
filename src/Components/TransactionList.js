7111885ef09cd759e25783aed944669b4d522dc9
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-17 02:09:40 +0530

    add useContext

--- src/Components/TransactionList.js
+++ src/Components/TransactionList.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React,{ useContext } from 'react'
+import { GlobalContext } from '../contexts/GlobalState'
 
 function TransactionList() {
+    const data = useContext(GlobalContext);
+    console.log(data)
     return (
         <>
             <h3>History</h3>
