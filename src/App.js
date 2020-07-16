df66e8beaa9eba7e47bd8dc1acf2d522d49b0a37
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-17 01:07:43 +0530

    add incomeExpenses comp to app comp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Balance from './Components/Balance';
+import IncomeExpenses from './Components/IncomeExpenses';
 
 function App() {
   return (
     <div>
       <Header />
       <Balance />
+      <IncomeExpenses />
     </div>
   );
 }
