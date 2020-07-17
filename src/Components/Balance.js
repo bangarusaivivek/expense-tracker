c2c9e16d37ba361ee4272ea895189c812146b69e
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-17 15:31:18 +0530

    updated dynamic balance

--- src/Components/Balance.js
+++ src/Components/Balance.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React,{ useContext } from 'react'
+import { GlobalContext } from '../contexts/GlobalState'
+
 
 function Balance() {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount);
+    // console.log(amounts)
+    const total = amounts.reduce((acc,item) => (acc += item),0).toFixed(2);
+    // console.log(total)
     return (
         <div className="container">
             <h4>Your Balance</h4>
-            <h1 id="balance">$0.00</h1>
+            <h1 id="balance">${total}</h1>
         </div>
     )
 }
