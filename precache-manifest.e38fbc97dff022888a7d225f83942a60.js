c63ab09ca8bf9a369d03c839864f628b409f12ab
bangarusaivivek <bangarusaivivek1996@gmail.com>
2020-07-18 00:31:55 +0530

    Updates

---
+++ precache-manifest.e38fbc97dff022888a7d225f83942a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea949eaad9bf71bd23d90d56df94bc7",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "4db7573fb0c02d275222",
+    "url": "/expense-tracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "cc6c33ff295d07bbd263",
+    "url": "/expense-tracker/static/js/2.c594d9bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.c594d9bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4db7573fb0c02d275222",
+    "url": "/expense-tracker/static/js/main.6277b6ea.chunk.js"
+  },
+  {
+    "revision": "7a228fdbfe8715a85308",
+    "url": "/expense-tracker/static/js/runtime-main.91d22b83.js"
+  }
+]);
